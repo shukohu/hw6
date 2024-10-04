3e59adab5efcf01a64c77317526a200b3e1988d5
Shukohu <dashievaviktoria06@gmail.com>
2024-10-04 15:59:45 +0800

    set

--- src/main/java/com/employee/service/EmployeeService.java
+++ src/main/java/com/employee/service/EmployeeService.java
@@ -2,10 +2,15 @@ package com.employee.service;
 
 import com.employee.Employee;
 
+import java.util.Collection;
+
 public interface EmployeeService {
     Employee add(String firstName, String lastName);
 
     Employee remove(String firstName, String lastName);
 
     Employee find(String firstName, String lastName);
+
+    Collection<Employee> findAll();
+
 }
